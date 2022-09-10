2e1ac35cdf254bac4d7be402ad89cc92976579ca
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-09-10 17:04:33 +0200

    chore: create new class and test AmountParser

--- src/Project/Parsers/AmountParser.php
+++ src/Project/Parsers/AmountParser.php
@@ -5,38 +5,84 @@ declare(strict_types=1);
 namespace App\Project\Parsers;
 
 use App\Project\Entities\Amount;
+use App\Project\Entities\Time;
 use App\Project\Interfaces\AmountParserInterface;
+use App\Project\Interfaces\TimeParserInterface;
 use Exception;
 
 class AmountParser implements AmountParserInterface
 {
+    /**
+     * @var TimeParaserInterface
+     */
+    private $timeParser;
+
+    public function __construct(TimeParserInterface $timeParser)
+    {
+        $this->timeParser = $timeParser;
+    }
+
     public function parse(string $string): Amount
     {
-        $amount = new Amount();
-
-        $valueStr = '';
-        $value = 0.0;
-        $type = Amount::TYPE_TIME;
-
-        if (strpos($string, 'min') !== false) {
-            $valueStr = str_replace('min', '', $string);
-            $value = floatval($valueStr);
-            $type = Amount::TYPE_TIME;
-        } elseif (strpos($string, 'h') !== false) {
-            $valueStr = str_replace('h', '', $string);
-            $value = floatval($valueStr) * 60;
-            $type = Amount::TYPE_TIME;
-        } elseif (strpos($string, 'x') !== false) {
-            $valueStr = str_replace('x', '', $string);
-            $value = floatval($valueStr);
-            $type = Amount::TYPE_FIX;
-        } else {
-            throw new Exception("Unkown unit in Qunatity field");
+        if ($this->isTime($string)) {
+            $amount = $this->parseTime($string);
+            return $amount;
+        } elseif ($this->isQuantity($string)) {
+            $amount = $this->parseQuantity($string);
+            return $amount;
         }
 
-        $amount->type = $type;
-        $amount->value = $value;
+        throw new Exception("Can not parse Amount");
+    }
 
+
+    private function parseTime(string $string): Amount
+    {
+        $amount = new Amount();
+        $amount->value = $this->timeParser->parse($string);
+        $amount->type = Amount::TYPE_TIME;
         return $amount;
+    }
+
+    private function parseQuantity(string $string): Amount
+    {
+        $string = str_replace('x', '', $string);
+        $string = str_replace('Stk', '', $string);
+        $string = trim($string);
+
+        $amount = new Amount();
+        $amount->value = $string;
+        $amount->type = Amount::TYPE_QUANTITY;
+        return $amount;
+    }
+
+    private function isTime(string $string): bool
+    {
+        if (strpos($string, 'min') !== false) {
+            return true;
+        }
+
+        if (strpos($string, 'h') !== false) {
+            return true;
+        }
+
+        if (strpos($string, ':') !== false) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function isQuantity(string $string): bool
+    {
+        if (strpos($string, 'x') !== false) {
+            return true;
+        }
+
+        if (strpos($string, 'Stk') !== false) {
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ tests/AmountParserTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use App\Project\Entities\Time;
+use App\Project\Entities\Amount;
+use App\Project\Parsers\AmountParser;
+use App\Project\Parsers\TimeParser;
+use PHPUnit\Framework\TestCase;
+
+final class AmountParserTest extends TestCase
+{
+    private $amountParser;
+
+    public function setUp(): void
+    {
+        $timeParser = new TimeParser();
+        $this->amountParser = new AmountParser($timeParser);
+    }
+
+    public function testCanParseAmountTimeHour(): void
+    {
+        $amount = $this->amountParser->parse('2,4h');
+
+        $expectedTime = new Time();
+        $expectedTime->value = '2,4';
+        $expectedTime->type = Time::TYPE_HOUR;
+
+        $expectedAmount = new Amount();
+        $expectedAmount->value = $expectedTime;
+        $expectedAmount->type = Amount::TYPE_TIME;
+
+        $this->assertEquals($expectedAmount, $amount);
+    }
+
+    public function testCanParseAmountTimeMinute(): void
+    {
+        $amount = $this->amountParser->parse('36min');
+
+        $expectedTime = new Time();
+        $expectedTime->value = '36';
+        $expectedTime->type = Time::TYPE_MINUTE;
+
+        $expectedAmount = new Amount();
+        $expectedAmount->value = $expectedTime;
+        $expectedAmount->type = Amount::TYPE_TIME;
+
+        $this->assertEquals($expectedAmount, $amount);
+    }
+
+    public function testCanParseAmountTimeMixed(): void
+    {
+        $amount = $this->amountParser->parse('2:30');
+
+        $expectedTime = new Time();
+        $expectedTime->value = '2:30';
+        $expectedTime->type = Time::TYPE_MIXED;
+
+        $expectedAmount = new Amount();
+        $expectedAmount->value = $expectedTime;
+        $expectedAmount->type = Amount::TYPE_TIME;
+
+        $this->assertEquals($expectedAmount, $amount);
+    }
+
+    public function testCanParseAmountQuantity(): void
+    {
+        $amount = $this->amountParser->parse('10x');
+
+        $expectedAmount = new Amount();
+        $expectedAmount->value = '10';
+        $expectedAmount->type = Amount::TYPE_QUANTITY;
+
+        $this->assertEquals($expectedAmount, $amount);
+    }
+}
