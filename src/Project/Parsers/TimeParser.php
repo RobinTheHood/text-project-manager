3fdb5915bcf1f3a7af393cbb7b3d9443f4cfd92f
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-09-10 17:06:48 +0200

    chore: update class TimeParser and create test

--- src/Project/Parsers/TimeParser.php
+++ src/Project/Parsers/TimeParser.php
@@ -4,26 +4,75 @@ declare(strict_types=1);
 
 namespace App\Project\Parsers;
 
+use App\Project\Entities\Time;
 use App\Project\Interfaces\TimeParserInterface;
 use Exception;
 
 class TimeParser implements TimeParserInterface
 {
-    public function parse(string $string): array
+    public function parse(string $string): Time
     {
         if (strpos($string, 'min') !== false) {
-            $unit = 'min';
-            $value = str_replace('min', '', $string);
+            return $this->parseMinute($string);
         } elseif (strpos($string, 'h') !== false) {
-            $unit = 'h';
-            $value = str_replace('h', '', $string);
+            return $this->parseHour($string);
+        } elseif (strpos($string, ':') !== false) {
+            return $this->parseMixed($string);
         } else {
-            throw new Exception("Unkown unit");
+            throw new Exception("Unkown Timeformat");
+        }
+    }
+
+    private function parseMinute(string $string): Time
+    {
+        $string = str_replace('min', '', $string);
+        $value = $this->parseValue($string);
+
+        $time = new Time();
+        $time->type = Time::TYPE_MINUTE;
+        $time->value = $value;
+
+        return $time;
+    }
+
+    private function parseHour(string $string): Time
+    {
+        $string = str_replace('h', '', $string);
+        $value = $this->parseValue($string);
+
+        $time = new Time();
+        $time->type = Time::TYPE_HOUR;
+        $time->value = $value;
+
+        return $time;
+    }
+
+    private function parseMixed(string $string): Time
+    {
+        $value = $this->parseValue($string);
+
+        $time = new Time();
+        $time->type = Time::TYPE_MIXED;
+        $time->value = $value;
+
+        return $time;
+    }
+
+    private function parseValue(string $string): string
+    {
+        $string = trim($string);
+
+        $fistChar = mb_substr($string, 0, 1, 'utf-8');
+        $lastChar = mb_substr($string, -1, 1, 'utf-8');
+
+        if (!is_numeric($fistChar)) {
+            throw new Exception('Unkown Timeformat');
         }
 
-        return [
-            'value' => $value,
-            'unit' => $unit
-        ];
+        if (!is_numeric($lastChar)) {
+            throw new Exception('Unkown Timeformat');
+        }
+
+        return $string;
     }
 }
---
+++ tests/TimeParserTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use App\Project\Entities\Time;
+use App\Project\Parsers\TimeParser;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+final class TimeParserTest extends TestCase
+{
+    public function testCanParseTimeMinutes(): void
+    {
+        $timeParser = new TimeParser();
+        $time = $timeParser->parse('13min');
+
+        $expectedTime = new Time();
+        $expectedTime->value = '13';
+        $expectedTime->type = Time::TYPE_MINUTE;
+
+        $this->assertEquals($expectedTime, $time);
+    }
+
+    public function testCanParseUnformatedTimeMinutes(): void
+    {
+        $timeParser = new TimeParser();
+        $time = $timeParser->parse(' 13 min');
+
+        $expectedTime = new Time();
+        $expectedTime->value = '13';
+        $expectedTime->type = Time::TYPE_MINUTE;
+
+        $this->assertEquals($expectedTime, $time);
+    }
+
+    public function testCanParseTimeHours(): void
+    {
+        $timeParser = new TimeParser();
+
+        $time = $timeParser->parse('13,7h');
+        $expectedTime = new Time();
+        $expectedTime->value = '13,7';
+        $expectedTime->type = Time::TYPE_HOUR;
+        $this->assertEquals($expectedTime, $time);
+    }
+
+    public function testCanParseUnformatedTimeHours(): void
+    {
+        $timeParser = new TimeParser();
+        $time = $timeParser->parse(' 13,7 h');
+
+        $expectedTime = new Time();
+        $expectedTime->value = '13,7';
+        $expectedTime->type = Time::TYPE_HOUR;
+
+        $this->assertEquals($expectedTime, $time);
+    }
+
+    public function testCanParseTimeMixed(): void
+    {
+        $timeParser = new TimeParser();
+
+        $time = $timeParser->parse('13:60');
+        $expectedTime = new Time();
+        $expectedTime->value = '13:60';
+        $expectedTime->type = Time::TYPE_MIXED;
+        $this->assertEquals($expectedTime, $time);
+    }
+
+    public function testTimeParserThrowsException(): void
+    {
+        $this->expectException(Exception::class);
+
+        $timeParser = new TimeParser();
+        $time = $timeParser->parse(' 13 min.');
+    }
+}
