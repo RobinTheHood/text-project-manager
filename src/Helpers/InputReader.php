372d9f43d00be0713c1bac378d2e97200993f3b8
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-09-14 10:06:17 +0200

    chore: improve InputReader

--- src/Helpers/InputReader.php
+++ src/Helpers/InputReader.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace RobinTheHood\TextProjectManager\Helpers;
 
 use RobinTheHood\TextProjectManager\Adapters\FileGetContentsWrapperInterface;
-use Exception;
 
-class InputReader
+class InputReader implements InputReaderInterface
 {
     /**
      * @var string
@@ -38,28 +37,33 @@ class InputReader
 
     public function seek(int $count = 1): string
     {
-        if ($this->isEof($this->pointer + $count)) {
-            throw new Exception("End of File");
-        }
-        return substr($this->content, $this->pointer, $count);
+        return $this->getSubContent($this->pointer, $count);
     }
 
     public function consume(int $count = 1): string
     {
-        if ($this->isEof($this->pointer + $count)) {
-            throw new Exception("End of File");
-        }
-
-        $string = substr($this->content, $this->pointer, $count);
+        $string = $this->getSubContent($this->pointer, $count);
         $this->pointer += $count;
         return $string;
     }
 
-    private function isEof(int $offset): bool
+    public function isEof(): bool
     {
-        if ($offset > $this->contentLength) {
+        if ($this->pointer >= $this->contentLength) {
             return true;
         }
         return false;
+    }
+
+    private function getSubContent(int $offset, int $length): string
+    {
+        if ($offset > $this->contentLength) {
+            return "";
+        }
+
+        $maxLength = $this->contentLength - $offset;
+        $useLength = min($length, $maxLength);
+
+        return substr($this->content, $offset, $useLength);
     }
 }
---
+++ src/Helpers/InputReaderInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RobinTheHood\TextProjectManager\Helpers;
+
+interface InputReaderInterface
+{
+    public function seek(int $count = 1): string;
+
+    public function consume(int $count = 1): string;
+
+    public function isEof(): bool;
+}
