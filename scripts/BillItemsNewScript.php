82c330a5bd3138f53483f29a46adcfc2832d7c97
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-09-16 17:57:59 +0200

    chore: add new BillItemsCreator

--- scripts/BillItemsNewScript.php
+++ scripts/BillItemsNewScript.php
@@ -5,12 +5,10 @@ namespace RobinTheHood\TextProjectManager\Scripts;
 use Exception;
 use RobinTheHood\TextProjectManager\Adapters\FileGetContentsWrapper;
 use RobinTheHood\TextProjectManager\Helpers\InputReader;
+use RobinTheHood\TextProjectManager\Project\Creators\BillItemsNewCreator;
 use RobinTheHood\TextProjectManager\Project\Lexer\Lexer;
-use RobinTheHood\TextProjectManager\Project\Lexer\Token;
 use RobinTheHood\TextProjectManager\Project\ParsersNew\ProjectParser;
 use RobinTheHood\TextProjectManager\Project\ParsersNew\Parser;
-use RobinTheHood\TextProjectManager\Project\ParsersNew\TaskParser;
-use RobinTheHood\TextProjectManager\Project\ParsersNew\TimeParser;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -19,14 +17,14 @@ $inputReader = new InputReader($fileGetsContentWrapper, __DIR__ . '/../data/Pars
 $lexer = new Lexer($inputReader);
 $parser = new Parser($lexer);
 
-// $taskParser = new TaskParser();
-// $task = $taskParser->parse($parser);
-// var_dump($task);
-
 try {
     $projectParser = new ProjectParser();
     $project = $projectParser->parse($parser);
     var_dump($project);
+
+    $billItemNewCreatpr = new BillItemsNewCreator();
+    $string = $billItemNewCreatpr->create($project);
+    echo $string;
 } catch (Exception $e) {
     echo $e;
 }
