0291cabcfb42387091f381153cfdc87ebe0808f5
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-09-21 19:25:07 +0200

    chore: add new script EvaliationNewScript

---
+++ scripts/EvaluationNewScript.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RobinTheHood\TextProjectManager\Scripts;
+
+use Exception;
+use RobinTheHood\TextProjectManager\Adapters\FileGetContentsWrapper;
+use RobinTheHood\TextProjectManager\Helpers\InputReader;
+use RobinTheHood\TextProjectManager\Project\Creators\EvaluationCreator;
+use RobinTheHood\TextProjectManager\Project\Lexer\Lexer;
+use RobinTheHood\TextProjectManager\Project\Parsers\ProjectParser;
+use RobinTheHood\TextProjectManager\Project\Parsers\Parser;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$fileGetsContentWrapper = new FileGetContentsWrapper();
+$inputReader = new InputReader($fileGetsContentWrapper, __DIR__ . '/../data/ParserTest.md');
+$lexer = new Lexer($inputReader);
+$parser = new Parser($lexer);
+
+try {
+    $projectParser = new ProjectParser();
+    $project = $projectParser->parse($parser);
+    //var_dump($project);
+
+    $evaluationCreator = new EvaluationCreator();
+    $string = $evaluationCreator->create($project);
+    echo $string;
+} catch (Exception $e) {
+    echo $e;
+}
