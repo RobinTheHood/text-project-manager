4fec19c5726900650ff1d2630b05838f3983e087
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-07-13 12:54:23 +0200

    feat: add rate to ReportCondensateFactory

--- src/Project/Creators/ReportCondensateFactory.php
+++ src/Project/Creators/ReportCondensateFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RobinTheHood\TextProjectManager\Project\Creators;
 
+use DateTime;
 use RobinTheHood\TextProjectManager\Project\Entities\Duration;
 use RobinTheHood\TextProjectManager\Project\Entities\Quantity;
 use RobinTheHood\TextProjectManager\Project\Entities\Report;
@@ -44,8 +45,17 @@ class ReportCondensateFactory
             $externalQuantity = $this->stepRoundMinutes($minutes, 15) / 60;
         } elseif ($report->amount->value instanceof Quantity) {
             $type = ReportCondensate::TYPE_QUANTITY;
-            $internalQuantity = $report->amount->value->value;
-            $externalQuantity = $report->amount->value->value;
+            $quantity = $report->amount->value;
+
+            $qunatityValue = $this->getQuantityFromQuantity(
+                $quantity->value,
+                $quantity->rate ?? '',
+                $report->date,
+                date('d.m.Y')
+            );
+
+            $internalQuantity = $qunatityValue;
+            $externalQuantity = $qunatityValue;
         }
 
         $reports = [$report];
@@ -81,5 +91,24 @@ class ReportCondensateFactory
     {
         $roundedMinutes = ceil($minutes / $step) * $step;
         return $roundedMinutes;
+    }
+
+    private function getQuantityFromQuantity(float $value, string $rate, string $start, string $end): float
+    {
+        if ($rate != 'Monat') {
+            return $value;
+        }
+
+        $startDate = DateTime::createFromFormat('d.m.Y', $start);
+        $endDate = DateTime::createFromFormat('d.m.Y', $end);
+
+        // Berechnung der Differenz in Monaten
+        $interval = $endDate->diff($startDate);
+        $months = $interval->y * 12 + $interval->m;
+
+        // Multiplizieren mit dem Wert
+        $result = $value * $months;
+
+        return $result;
     }
 }
