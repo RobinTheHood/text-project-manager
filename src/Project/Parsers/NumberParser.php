30442ef9143ca3b811e2d3fd21f163574d2c6805
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-07-13 12:52:26 +0200

    feat: add rate to Parser

--- src/Project/Parsers/NumberParser.php
+++ src/Project/Parsers/NumberParser.php
@@ -10,9 +10,40 @@ use RobinTheHood\TextProjectManager\Project\Lexer\Token;
 class NumberParser
 {
     /**
-     * <number> ::= <token_int> <token_unit> | <token_float> <token_unit> | <token_int> | <token_float>
+     * <number> ::=
+     *     <token_int> <token_unit> <token_separater> <token_unit> |
+     *     <token_float> <token_unit> <token_separater> <token_unit> |
+     *     <token_int> <token_unit> |
+     *     <token_float> <token_unit> |
+     *     <token_int> <token_separater> <token_unit> |
+     *     <token_float> <token_separater> <token_unit> |
+     *     <token_int> |
+     *     <token_float>
      */
     public function parse(Parser $parser): ?Number
+    {
+        $number = null;
+
+        if (!$number) {
+            $number = $this->parseInt($parser);
+        }
+
+        if (!$number) {
+            $number = $this->parseFloat($parser);
+        }
+
+        if ($number) {
+            if ($token = $parser->accept(Token::TYPE_SEPARATOR, '/')) {
+                if ($token = $parser->accept(Token::TYPE_UNIT)) {
+                    $number->rate = $token->string;
+                }
+            }
+        }
+
+        return $number;
+    }
+
+    private function parseInt(Parser $parser): ?Number
     {
         $number = new Number();
 
@@ -21,8 +52,16 @@ class NumberParser
             if ($token = $parser->accept(Token::TYPE_UNIT)) {
                 $number->unit = $token->string;
             }
+
             return $number;
         }
+
+        return null;
+    }
+
+    private function parseFloat(Parser $parser): ?Number
+    {
+        $number = new Number();
 
         if ($token = $parser->accept(Token::TYPE_FLOAT)) {
             $number->value = $this->toFloat($token->string);
